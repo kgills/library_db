fad1baeb8d4286dec09aea192e96d697a5d71e22
Kevin <kgills@gmail.com>
2017-07-10 08:50:30 -0500

    Finished with the db processing. Adding smaller book file. Cleaning up some of the authors.

--- create_db.py
+++ create_db.py
@@ -38,40 +38,88 @@ cursor.execute("CREATE DATABASE library")
 cursor.execute("USE library")
 
 # Create the borrowers table
-cursor.execute("CREATE TABLE BORROWER ( Card_id int NOT NULL, Ssn int NOT NULL, Bname varchar(100) NOT NULL, Address varchar(100) NOT NULL, Phone bigint NOT NULL, PRIMARY KEY (Card_id))")
+cursor.execute("""CREATE TABLE BORROWER ( 
+	Card_id int NOT NULL, 
+	Ssn int NOT NULL UNIQUE, 
+	Bname varchar(100) NOT NULL, 
+	Address varchar(100) NOT NULL, 
+	Phone bigint NOT NULL, 
 
-# Sample borrower
-# cursor.execute("INSERT INTO BORROWER VALUES('1', '850473740', 'Mark Morgan', '5677 Coolidge Street,Plano,TX', '4699041438')")
+	PRIMARY KEY (Card_id))
+""")
+
+# Create the Authors table
+cursor.execute("""CREATE TABLE AUTHORS ( 
+	Author_id bigint NOT NULL, 
+	Name varchar(100) NOT NULL,
+
+	PRIMARY KEY (Author_id))
+""")
+
+# Create the Book table
+cursor.execute("""CREATE TABLE BOOK ( 
+	Isbn bigint NOT NULL, 
+	Title varchar(250) NOT NULL, 
+
+	PRIMARY KEY (Isbn))
+""")
+
+# Create the Book_authors table
+cursor.execute("""CREATE TABLE BOOK_AUTHORS ( 
+	Author_id bigint NOT NULL, 
+	Isbn bigint NOT NULL, 
+ 
+	FOREIGN KEY (Author_id) 
+		REFERENCES AUTHORS(Author_id) 
+		ON DELETE CASCADE)
+""")
+
+# Create the Book_Loans table
+cursor.execute("""CREATE TABLE BOOK_LOANS ( 
+	Loan_id bigint NOT NULL, 
+	Isbn bigint NOT NULL, 
+	Card_id bigint NOT NULL, 
+	Date_out DATE NOT NULL, 
+	Due_date DATE NOT NULL, 
+	Date_in DATE, 
+
+	PRIMARY KEY (Loan_id))
+""")
+
+# Creat the fines table
+cursor.execute("""CREATE TABLE FINES ( 
+	Loan_id bigint NOT NULL, 
+	Fine_amt DECIMAL(10,2), 
+	Paid BOOLEAN NOT NULL,
+
+	PRIMARY KEY (Loan_id),
+	FOREIGN KEY (Loan_id) 
+	 	REFERENCES BOOK_LOANS(Loan_id) 
+	 	ON DELETE CASCADE)
+""")
 
 # Read the entries from the borrowers CSV
 with open(borrow_file, 'rb') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',')
 	for row in reader:
 
-		# print row[0]+" | "+row[1]+" | "+row[2]+" | "+row[3]+" | "+row[4]+" | "+row[5]+" | "+row[6]+" | "+row[7]+" | "+row[8]
-
 		# Strip ID, convert to int, back to string
 		card_id = str(int(row[0][2:]))
-		# print "Card_id = "+card_id
 
 		# Strip the - characters
 		ssn = row[1].replace("-", "")
-		# print "SSN = "+ssn
 
 		# Combine the names
 		name = row[2]+" "+row[3]
-		# print "Name = "+name
 
 		# Combine the address, skip email
 		addr = row[5]+", "+row[6]+", "+row[7]
-		# print "Addr = "+addr
 
 		# Strip non digits from the phone number
 		phone=row[8].replace("(", "")
 		phone=phone.replace(")", "")
 		phone=phone.replace(" ", "")
 		phone=phone.replace("-", "")
-		# print "Phone = "+phone
 
 		cursor.execute("INSERT INTO BORROWER VALUES('"+card_id+"', '"+ssn+"', '"+name+"', '"+addr+"', '"+phone+"')")
 
@@ -79,32 +127,12 @@ with open(borrow_file, 'rb') as csvfile:
 # Commit changes
 db.commit()
 
-# Get the number of rows in the resultset
-# cursor.execute("SELECT * FROM BORROWER")
-# numrows = cursor.rowcount
-
-# Get and display one row at a time
-# for x in range(0, numrows):
-    # row = cursor.fetchone()
-    # print row[0], "|", row[1], "|", row[2], "|", row[3], "|", row[4]
-
-
-# Create the Authors table
-cursor.execute("CREATE TABLE AUTHORS ( Author_id bigint NOT NULL, Name varchar(100) NOT NULL , PRIMARY KEY (Author_id))")
-
-# Create the Book table
-cursor.execute("CREATE TABLE BOOK ( Isbn bigint NOT NULL, Title varchar(250) NOT NULL, PRIMARY KEY (Isbn))")
-
-# Create the Book_authors table
-cursor.execute("CREATE TABLE BOOK_AUTHORS ( Author_id bigint NOT NULL, Isbn bigint NOT NULL, PRIMARY KEY (Author_id))")
-
 author_id = 1
 
 # Read the entries from the books CSV
 with open(book_file, 'rb') as csvfile:
 	reader = csv.reader(csvfile, delimiter='\t')
 	for row in reader:
-		# print row[0]+" | "+row[1]+" | "+row[2]+" | "+row[3]+" | "+row[4]+" | "+row[5]+" | "+row[6]
 
 		# Use ISBN13 for the book ISBN
 		isbn = row[1]
@@ -116,7 +144,8 @@ with open(book_file, 'rb') as csvfile:
 		authors = row[3].split(",")
 
 		for author in authors: 
-
+	
+			this_author_id = 0
 			if(author == ""):
 				continue
 
@@ -142,87 +171,59 @@ with open(book_file, 'rb') as csvfile:
 				author_names.remove("")
 
 			
-
 			# Search for the author to see if we already have him/her in the DB
-			exe_string = "SELECT * from AUTHORS WHERE Name LIKE BINARY\'"
-			count = 0;
+			exe_string = "SELECT * from AUTHORS WHERE Name LIKE BINARY \'"
+			count = 1;
 			if(len(author_names) < 3):
 				exe_string=exe_string+author
-
 			else:
 				for name in author_names:
-					if(count < 1):
-						exe_string=exe_string+name[0]+"%"
+					if(count < len(author_names)):
+						exe_string=exe_string+name[0]+"% "
 					else:
-						exe_string=exe_string+name+"%"
+						exe_string=exe_string+name
 					count = count+1
-
+			
 			exe_string=exe_string+"\'"
+			# print exe_string
 			cursor.execute(exe_string)
 
 			# Get the number of rows in the resultset
 			numrows = cursor.rowcount
-
 			if(numrows == 2):
-				# More than one match, we need to merge the authors
-				print ""
+				# More than one match, we need to merge the 3 author names
+				print "Need to merge the rows"
 				print author
 				print exe_string
-				# Get and display one row at a time
-				# for x in range(0, numrows):
-				#     row = cursor.fetchone()
-				#     print row[0], "|", row[1]
 
-
-				author_len = 0
-				author_large_id = 0
-				for x in range(0, numrows):
-					row = cursor.fetchone()
-
-					if(len(row[1]) > author_len):
-						author_len = len(row[1])
-						author_large_id = row[0]
-
-				print author_large_id
-
+				row0 = cursor.fetchone()
+				row1 = cursor.fetchone()
+				print row0[1]+" "+str(row0[0])+"  | "+row1[1]+" "+str(row1[0])
+				
 			elif(numrows == 0):
 				# Add this author
 				exe_string = "INSERT INTO AUTHORS VALUES('"+str(author_id)+"', '"+author+"')"
+				this_author_id = str(author_id)
 				author_id = author_id +1
 				# print exe_string
 				cursor.execute(exe_string)
-
-			# Add Author if necessary to the AUTHORS table
+			elif(numrows > 1):
+				print "Too Many Rows "+str(numrows)
+			else:
+				# This author already exists
+				row = cursor.fetchone()
+				this_author_id = str(row[0])
+				
 
 			# Add the Author to the BOOK_AUTHORS table
-
-
-		# print "Authors = "+"|".join(authors)
-
-		# print "isbn = "+isbn+" title = "+title
+			exe_string = "INSERT INTO BOOK_AUTHORS VALUES('"+this_author_id+"', '"+str(isbn)+"')"
+			cursor.execute(exe_string)
 
 		cursor.execute("INSERT INTO BOOK VALUES('"+isbn+"', '"+title+"')")
 
 
 # Commit changes
 db.commit()
-
-# Get the number of rows in the resultset
-# cursor.execute("SELECT * FROM BOOK")
-# numrows = cursor.rowcount
-
-# # Get and display one row at a time
-# for x in range(0, numrows):
-    # row = cursor.fetchone()
-    # print row[0], "|", row[1]
-
-# Add authors
-
-
-# Create the book_autors table
-
-# Populate the Book_authors table
-
 
 # Close the connection
 db.close()
